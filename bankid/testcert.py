7f4a5b716d6ff0f83ad79d9633efc486e9fa42a1
Henrik Blidh <henrik.blidh@nedomkull.com>
2014-09-11 08:27:56 +0200

    Version 0.1.1
    Testcert file modified and documented.

--- bankid/testcert.py
+++ bankid/testcert.py
@@ -19,6 +19,7 @@ from __future__ import print_function
 from __future__ import absolute_import
 
 import os
+import re
 import tempfile
 import subprocess
 import sys
@@ -29,12 +30,30 @@ _CERT_URL = "http://www.bankid.com/Global/wwwbankidcom/RP/FPTestcert1.pfx"
 
 
 def create_test_server_cert_and_key(destination_path):
+    """Fetch the Pfx certificate from BankID servers, split it into
+    a certificate part and a key part and save them as separate files,
+    stored in PEM format.
+
+    :param destination_path: The directory to save certificate and key to.
+    :type destination_path: unicode
+    :returns: The path tuple ``(cert_path, key_path)``.
+    :rtype: tuple
+
+    """
     if sys.platform == 'win32':
-        raise NotImplementedError("Test certificate fetching in Windows not supported. "
-                                  "See documentation for details.")
+        raise NotImplementedError(
+            "Test certificate fetching in Windows not supported. "
+            "See documentation for details.")
+
+    # Paths to temporary files.
     cert_tmp_path = os.path.abspath(
         os.path.join(tempfile.gettempdir(),
                      os.path.basename(_CERT_URL)))
+    conv_tmp_path = os.path.abspath(
+        os.path.join(tempfile.gettempdir(),
+                     'certificate.pem'))
+
+    # Paths to output files.
     out_cert_path = os.path.abspath(
         os.path.join(os.path.abspath(destination_path),
                      'cert.pem'))
@@ -42,28 +61,51 @@ def create_test_server_cert_and_key(destination_path):
         os.path.join(os.path.abspath(destination_path),
                      'key.pem'))
 
+    # Fetch pfx certificate and store in temporary folder.
     r = requests.get(_CERT_URL)
     with open(cert_tmp_path, 'wb') as f:
         f.write(r.content)
-    p = subprocess.Popen(['openssl', 'pkcs12',
-                          '-in', "'{0}'".format(cert_tmp_path),
-                          '-passin', 'pass:{0}'.format(_TEST_CERT_PASSWORD),
-                          '-out', "'{0}'".format(out_cert_path),
-                          '-passout', 'pass:{0}'.format(_TEST_CERT_PASSWORD)])
-    p = subprocess.Popen(['openssl', 'pkcs12',
-                          '-in', "'{0}'".format(cert_tmp_path),
-                          '-passin', 'pass:{0}'.format(_TEST_CERT_PASSWORD),
-                          '|', 'openssl', 'rsa', '-des3',
-                          '-out', "'{0}'".format(out_key_path),
-                          '-passout', 'pass:{0}'.format(_TEST_CERT_PASSWORD)])
 
-    # TODO: Check if processes exited correctly.
+    # Use openssl for converting to pem format.
+    pipeline_1 = [
+        'openssl', 'pkcs12',
+        '-in', "{0}".format(cert_tmp_path),
+        '-passin', 'pass:{0}'.format(_TEST_CERT_PASSWORD),
+        '-out', "{0}".format(conv_tmp_path),
+        '-passout', 'pass:{0}'.format(_TEST_CERT_PASSWORD)]
+    p = subprocess.Popen(pipeline_1, stdout=subprocess.PIPE)
+    p.communicate()
 
+    # Open the newly created pem certificate in temporary folder.
+    with open(conv_tmp_path, 'rt') as f:
+        cert_and_key = f.read()
+
+    # Split it into a certificate part and a private key part.
+    # Save these parts to separate files.
+    s = re.search('-----END CERTIFICATE-----', cert_and_key)
+    certificate = cert_and_key[:s.end()]
+    key = cert_and_key[s.end():]
+
+    with open(out_cert_path, 'wt') as f:
+        f.write(certificate)
+    with open(out_key_path, 'wt') as f:
+        f.write(key)
+
+    # Try to remove all temporary files.
+    try:
+        os.remove(cert_tmp_path)
+        os.remove(conv_tmp_path)
+    except:
+        pass
+
+    # Return path tuples.
     return out_cert_path, out_key_path
 
 
 def main():
-    create_test_server_cert_and_key(os.path.expanduser('~'))
+    paths = create_test_server_cert_and_key(os.path.expanduser('~'))
+    print('Saved cerificate as {0}'.format(paths[0]))
+    print('Saved key as {0}'.format(paths[1]))
 
 if __name__ == "__main__":
     main()
