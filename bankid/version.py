b998e197ddff1a5a3500d9c6734b48cd4b222529
Henrik Blidh <henrik.blidh@nedomkull.com>
2017-11-17 07:55:41 +0100

    Version 0.6.1 bump

--- bankid/version.py
+++ bankid/version.py
@@ -12,5 +12,5 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import absolute_import
 
-__version__ = '0.6.1-alpha1'
+__version__ = '0.6.1'
 version = __version__  # backwards compatibility name
