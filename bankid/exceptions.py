1eb065a210f97ddd29cd24adc7be81e75503f93e
David Svenson <david@personalkollen.se>
2019-03-13 09:37:53 +0100

    Drop SOAP client

--- bankid/exceptions.py
+++ bankid/exceptions.py
@@ -18,14 +18,6 @@ from __future__ import absolute_import
 import six
 
 
-def get_error_class(exc, exception_text):
-    error_class = _ERROR_CODE_TO_CLASS.get(six.text_type(exc.message))
-    if error_class is None:
-        return BankIDError("{0}: {1}".format(exc, exception_text))
-    else:
-        return error_class(exception_text)
-
-
 def get_json_error_class(response):
     data = response.json()
     error_class = _JSON_ERROR_CODE_TO_CLASS.get(data.get("errorCode"), BankIDError)
@@ -33,11 +25,14 @@ def get_json_error_class(response):
 
 
 class BankIDError(Exception):
-    """Parent exception class for all PyBankID errors."""
+    """Parent exception class for all PyBankID errors.
+    
+    If an unknown error code is returned, RP should inform the user. 
+    Message RFA22 should be used. RP should update their implementation
+    to support the new error code as soon as possible. 
+    """
 
-    def __init__(self, *args, **kwargs):
-        super(BankIDError, self).__init__(*args, **kwargs)
-        self.rfa = None
+    RFA = 22
 
 
 class BankIDWarning(Warning):
@@ -47,9 +42,8 @@ class BankIDWarning(Warning):
 
 
 class InvalidParametersError(BankIDError):
-    """User induced error.
-
-    **Code:** ``INVALID_PARAMETERS``
+    """
+    **errorCode:** ``invalidParameters``
 
     **Reason:** Invalid parameter. Invalid use of method.
 
@@ -59,30 +53,28 @@ class InvalidParametersError(BankIDError):
 
     """
 
+    RFA = None
+
 
 class AlreadyInProgressError(BankIDError):
-    """Failure to create new order due to one already in progress.
-
-    **Code:** ``ALREADY_IN_PROGRESS``
+    """
+    **errorCode:** ``alreadyInProgress``
 
     **Reason:** An order for this user is already in progress. The order
     is aborted. No order is created.
 
     **Action by RP:** RP must inform the user that a login or signing
     operation is already initiated for this user.
-    Message RFA3 should be used.
+    Message RFA4 should be used.
 
     """
 
-    def __init__(self, *args, **kwargs):
-        super(AlreadyInProgressError, self).__init__(*args, **kwargs)
-        self.rfa = 3
+    RFA = 4
 
 
 class InternalError(BankIDError):
-    """Remote server error.
-
-    **Code:** ``INTERNAL_ERROR``
+    """
+    **errorCode:** ``internalError``
 
     **Reason:** Internal technical error in the BankID system.
 
@@ -92,13 +84,14 @@ class InternalError(BankIDError):
 
     """
 
-    def __init__(self, *args, **kwargs):
-        super(InternalError, self).__init__(*args, **kwargs)
-        self.rfa = 5
+    RFA = 5
 
 
 class MaintenanceError(BankIDError):
-    """The service is temporarily out of service.
+    """
+    **errorCode:** ``Maintenance``
+
+    **Reason:** The service is temporarily out of service.
 
     **Action by RP:** RP may try again without informing the user.
     If this error is returned repeatedly, RP must inform the user.
@@ -106,167 +99,14 @@ class MaintenanceError(BankIDError):
 
     """
 
-    def __init__(self, *args, **kwargs):
-        super(MaintenanceError, self).__init__(*args, **kwargs)
-        self.rfa = 5
-
-
-class RetryError(BankIDError):
-    """Remote server error, different from InternalError.
-
-    **Code:** ``RETRY``
-
-    **Reason:** Internal technical error in the BankID system.
-
-    **Action by RP:** RP must not automatically try again. RP must
-    inform the user that a technical error has
-    occurred. Message RFA5 should be used.
-
-    """
-
-    def __init__(self, *args, **kwargs):
-        super(RetryError, self).__init__(*args, **kwargs)
-        self.rfa = 5
-
-
-class AccessDeniedRPError(BankIDError):
-    """Access permission denied error.
-
-    **Code:** ``ACCESS_DENIED_RP``
-
-    **Reason:** RP does not have access to the service or
-    requested operation.
-
-    **Action by RP:** RP must not try the same request again. This is
-    an internal error within RP's system and must
-    not be communicated to the user as a BankID-error.
-
-    """
-
-
-class ClientError(BankIDError):
-    """Remote technical error.
-
-    **Code:** ``CLIENT_ERR``
-
-    **Reason:** Internal technical error. It was not possible to
-    create or verify the transaction.
-
-    **Action by RP:** RP must not automatically try again. RP must
-    inform the user. Message RFA12.
-
-    """
-
-    def __init__(self, *args, **kwargs):
-        super(ClientError, self).__init__(*args, **kwargs)
-        self.rfa = 12
-
-
-class ExpiredTransactionError(BankIDError):
-    """Error due to collecting on an expired order.
-
-    **Code:** ``EXPIRED_TRANSACTION``
-
-    **Reason:** The order has expired. The BankID security
-    app/program did not start, the user did not
-    finalize the signing or the RP called collect
-    too late.
-
-    **Action by RP:** RP must inform the user. Message RFA8.
-
-    """
-
-    def __init__(self, *args, **kwargs):
-        super(ExpiredTransactionError, self).__init__(*args, **kwargs)
-        self.rfa = 8
-
-
-class CertificateError(BankIDError):
-    """Error due to certificate issues.
-
-    **Code:** ``CERTIFICATE_ERR``
-
-    **Reason:**
-    This error is returned if:
-        1) The user has entered wrong security code
-           too many times in her mobile device. The
-           Mobile BankID cannot be used.
-        2) The users BankID is revoked.
-        3) The users BankID is invalid.
-
-    **Action by RP:** RP must inform the user. Message RFA3.
-
-    """
-
-    def __init__(self, *args, **kwargs):
-        super(CertificateError, self).__init__(*args, **kwargs)
-        self.rfa = 3
-
-
-class UserCancelError(BankIDError):
-    """User had issue a cancel on the order.
-
-    **Code:** ``USER_CANCEL``
-
-    **Reason:** The user decided to cancel the order.
-
-    **Action by RP:** RP must inform the user. Message RFA6.
-
-    """
-
-    def __init__(self, *args, **kwargs):
-        super(UserCancelError, self).__init__(*args, **kwargs)
-        self.rfa = 6
-
-
-class CancelledError(BankIDError):
-    """User had issue a cancel on the order.
-
-    **Code:** ``CANCELLED``
-
-    **Reason:** The order was cancelled. The system
-    received a new order for the user.
-
-    **Action by RP:** RP must inform the user. Message RFA3.
-
-    """
-
-    def __init__(self, *args, **kwargs):
-        super(CancelledError, self).__init__(*args, **kwargs)
-        self.rfa = 3
-
-
-class StartFailedError(BankIDError):
-    """Error handling the order's progression due to RP/user issues.
-
-    **Code:** ``START_FAILED``
-
-    **Reason:** The user did not provide her ID, or the RP
-    requires ``autostarttoken`` to be used, but the
-    client did not start within a certain time limit.
-    The reason may be:
-
-        1) RP did not use autoStartToken when starting
-           BankID security program/app.
-        2) The client software was not installed
-           or other problem with the user’s computer.
-
-    **Action by RP:**
-
-        1) The RP must use autoStartToken when
-           starting the client.
-        2) The RP must inform the user. Message RFA17.
-
-    """
-
-    def __init__(self, *args, **kwargs):
-        super(StartFailedError, self).__init__(*args, **kwargs)
-        # TODO: Dual cause, in which only one requires RFA. Remove?
-        self.rfa = 17
+    RFA = 5
 
 
 class UnauthorizedError(BankIDError):
-    """RP does not have access to the service.
+    """
+    **errorCode:** ``unauthorized``
+
+    **Reason:** RP does not have access to the service.
 
     **Action by RP:** RP must not try the same request again.
     This is an internal error within RP's system and must not be '
@@ -274,11 +114,14 @@ class UnauthorizedError(BankIDError):
 
     """
 
-    pass
+    RFA = None
 
 
 class NotFoundError(BankIDError):
-    """An erroneously URL path was used.
+    """
+    **errorCode:** ``notFound```
+    
+    **Reason:** An erroneously URL path was used.
 
     **Action by RP:** RP must not try the same request again.
     This is an internal error within RP's system and must not be '
@@ -286,11 +129,14 @@ class NotFoundError(BankIDError):
 
     """
 
-    pass
+    RFA = None
 
 
 class RequestTimeoutError(BankIDError):
-    """It took too long time to transmit the request.
+    """
+    **errorCode:** ``requestTimeout``
+    
+    **Reason:** It took too long time to transmit the request.
 
     **Action by RP:** RP must not automatically try again.
     This error may occur if the processing at RP or the communication is too
@@ -298,24 +144,7 @@ class RequestTimeoutError(BankIDError):
 
     """
 
-    def __init__(self, *args, **kwargs):
-        super(RequestTimeoutError, self).__init__(*args, **kwargs)
-        self.rfa = 5
-
-
-_ERROR_CODE_TO_CLASS = {
-    "INVALID_PARAMETERS": InvalidParametersError,
-    "ALREADY_IN_PROGRESS": AlreadyInProgressError,
-    "INTERNAL_ERROR": InternalError,
-    "RETRY": RetryError,
-    "ACCESS_DENIED_RP": AccessDeniedRPError,
-    "CLIENT_ERR": ClientError,
-    "EXPIRED_TRANSACTION": ExpiredTransactionError,
-    "CERTIFICATE_ERR": CertificateError,
-    "USER_CANCEL": UserCancelError,
-    "CANCELLED": CancelledError,
-    "START_FAILED": StartFailedError,
-}
+    RFA = 5
 
 
 _JSON_ERROR_CODE_TO_CLASS = {
--- docs/usage.rst
+++ docs/usage.rst
@@ -1,15 +1,10 @@
 Usage
 -----
 
-There are two different clients available in the :py:mod:`bankid` package: the
-:py:class:`bankid.client.BankIDClient`, which uses the SOAP-based API that is being deprecated
-in February 2020, and the :py:class:`bankid.jsonclient.BankIDJSONClient`, which uses the new
-JSON API released in February 2018.
-
-**Any new deployment using PyBankID should use the JSON Client!**
+:py:class:`bankid.jsonclient.BankIDJSONClient` is the client to be used to
+communicate with the BankID service. It uses the JSON API released in February 2018.
 
 .. toctree::
    :maxdepth: 2
 
    jsonclient
-   soapclient
