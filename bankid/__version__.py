9a1dccd7ba821b71070595aa164efc7128f979ae
Henrik Blidh <henrik.blidh@nedomkull.com>
2024-08-23 11:57:12 +0200

    Version 1.3.0 (#72)
    * Update test certificates
    
    * Using new test certs and removing old ones
    
    * Fixed pem cert and key
    
    The old exporting functions did not work any more apparently.
    Exported them from FPTestcert5_20240610.pem instead of p12.
    
    * Version bump
    
    ---------
    
    Co-authored-by: Emil <emil@personalkollen.se>

--- bankid/__version__.py
+++ bankid/__version__.py
@@ -3,5 +3,5 @@
 Version info
 """
 
-__version__ = "1.2.0"
+__version__ = "1.3.0"
 version = __version__  # backwards compatibility name
--- bankid/certs/__init__.py
+++ bankid/certs/__init__.py
@@ -6,11 +6,11 @@ from typing import Tuple
 
 
 def get_test_cert_p12() -> Path:
-    return (Path(__file__).parent / "FPTestcert4_20230629.p12").resolve()
+    return (Path(__file__).parent / "FPTestcert5_20240610.p12").resolve()
 
 
 def get_test_cert_and_key() -> Tuple[Path, Path]:
     return (
-        (Path(__file__).parent / "FPTestcert4_20230629_cert.pem").resolve(),
-        (Path(__file__).parent / "FPTestcert4_20230629_key.pem").resolve(),
+        (Path(__file__).parent / "FPTestcert5_20240610_cert.pem").resolve(),
+        (Path(__file__).parent / "FPTestcert5_20240610_key.pem").resolve(),
     )
